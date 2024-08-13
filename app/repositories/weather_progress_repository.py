d28c87893e0d99038d65568aa235026098871cc6
Bianka Passos <biankatpas@gmail.com>
2024-08-13 16:09:22 -0300

    added missing try catch

--- app/repositories/weather_progress_repository.py
+++ app/repositories/weather_progress_repository.py
@@ -2,13 +2,17 @@ class WeatherProgressRepository:
     def __init__(self, db_connection):
         self.db_connection = db_connection
 
-    # TODO: try catch
     def user_request_data_already_processed(self, uuid):
-        cursor = self.db_connection.cursor()
-        cursor.execute(
-            "SELECT COUNT(*) FROM progress WHERE user_request_id = ?", (uuid,)
-        )
-        return cursor.fetchone()[0]
+        try:
+            cursor = self.db_connection.cursor()
+            cursor.execute("SELECT COUNT(*) FROM progress WHERE user_request_id = ?", (uuid,))
+            result = cursor.fetchone()
+            return result[0] if result else 0
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return 0
+        finally:
+            cursor.close()
 
     def request_uuid_exists(self, uuid):
         try:
