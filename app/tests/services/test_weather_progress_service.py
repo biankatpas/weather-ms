30bf577266b840101a072fb5d91b8b1d7485dd86
Bianka Passos <biankatpas@gmail.com>
2024-08-14 14:40:08 -0300

    fixes at service tests

--- app/tests/services/test_weather_progress_service.py
+++ app/tests/services/test_weather_progress_service.py
@@ -3,6 +3,7 @@ import uuid
 
 from app.core.database import get_db_connection, initialize_db
 
+from app.repositories.weather_repository import WeatherRepository
 from app.repositories.weather_progress_repository import WeatherProgressRepository
 from app.repositories.request_repository import RequestRepository
 from app.services.weather_progress_service import WeatherProgressService
@@ -25,11 +26,12 @@ def weather_progress_service(db_connection):
     return WeatherProgressService(db_connection)
 
 
-def test_get_progress_status(weather_progress_service, db_connection):
+def test_service_get_progress_status(weather_progress_service, db_connection):
     mock_uuid = str(uuid.uuid4())
     mock_totals = 10
 
     request_repository = RequestRepository(db_connection)
+    weather_repository = WeatherRepository(db_connection)
     weather_progress_repository = WeatherProgressRepository(db_connection)
 
     request_repository.store_request_uuid_to_process(mock_uuid)
@@ -43,7 +45,7 @@ def test_get_progress_status(weather_progress_service, db_connection):
         }
     }
 
-    weather_progress_repository.store_weather_data_on_db(mock_uuid, mock_weather_data)
+    weather_repository.store_weather_data_on_db(mock_uuid, mock_weather_data)
 
     completed = weather_progress_repository.user_request_data_already_processed(mock_uuid)
 
@@ -52,18 +54,28 @@ def test_get_progress_status(weather_progress_service, db_connection):
     assert result_total == mock_totals
 
 
-def test_request_uuid_exists(weather_progress_service, db_connection):
+def test_service_request_uuid_exists(weather_progress_service, db_connection):
     mock_uuid = str(uuid.uuid4())
+    mock_weather_data = {
+        'id': 123,
+        'main': {
+            'temp': 25.5,
+            'humidity': 60
+        }
+    }
 
     request_repository = RequestRepository(db_connection)
     request_repository.store_request_uuid_to_process(mock_uuid)
+
+    weather_repository = WeatherRepository(db_connection)
+    weather_repository.store_weather_data_on_db(mock_uuid, mock_weather_data)
 
     exists = weather_progress_service.request_uuid_exists(mock_uuid)
 
     assert exists
 
 
-def test_request_uuid_not_exists(weather_progress_service):
+def test_service_request_uuid_not_exists(weather_progress_service):
     mock_uuid = str(uuid.uuid4())
 
     exists = weather_progress_service.request_uuid_exists(mock_uuid)
